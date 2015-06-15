09698224c2db7eb39f6cad023bc06c4b818aea91
ProgramFOX <programfox@hotmail.be>
2015-06-15 17:03:51 +0200

    Make it work for Python 3

--- setup.sh
+++ setup.sh
@@ -1,3 +1,6 @@
-sudo easy_install pip
-pip install appdirs
-pip install requests --upgrade
+sudo apt-get update
+sudo apt-get install python3-setuptools
+sudo easy_install3 pip
+sudo mv /usr/local/bin/pip /usr/local/bin/pip3
+pip3 install appdirs
+pip3 install requests --upgrade
