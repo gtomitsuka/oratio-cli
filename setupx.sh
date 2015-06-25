e47afe8eae68655d7269405ba89e3e1be1ca8d33
ByteCommander <ByteCommander@users.noreply.github.com>
2015-06-25 15:44:58 +0200

    absolute paths to pip3
    There was a problem with bash finding the pip3 file without restarting the terminal...

--- setupx.sh
+++ setupx.sh
@@ -53,8 +53,8 @@ if pip -V | grep -q "(python 2"
 fi 
 
 echo "*** Now installing the latest version of module "appdirs" for Python 3..."
-pip3 -q install appdirs
+/usr/local/bin/pip3 -q install appdirs
 echo "*** Now installing the latest version of module "requests" for Python 3..."
-pip3 -q install requests --upgrade
+/usr/local/bin/pip3 -q install requests --upgrade
 
 echo "*** FINISHED! You are now ready to use oratio-cli!"
