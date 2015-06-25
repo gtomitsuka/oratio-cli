9a06311a1d23eff95bd146acb1cc1bf35d4a5302
ByteCommander <ByteCommander@users.noreply.github.com>
2015-06-25 14:29:54 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 sudo apt-get update
 sudo apt-get install python3-setuptools
-sudo easy_install3 pip
-pip install appdirs
-pip install requests --upgrade
+sudo easy_install3 -U pip
+pip3 install appdirs
+pip3 install requests --upgrade
