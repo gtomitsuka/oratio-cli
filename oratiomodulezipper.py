1ba879397eccc2a0f998f9a603b525fc2c5f22f4
ProgramFOX <programfox@hotmail.be>
2015-06-26 15:26:06 +0200

    Some refactoring
    Rename compress_module -> compress_files
    Extract actual compress_module from compress()

--- oratiomodulezipper.py
+++ oratiomodulezipper.py
@@ -1,7 +1,18 @@
 import tarfile
+import os
+
+from oratioignoreparser import OratioIgnoreParser
 
 
-def compress_module(file_list, output_file):
+def compress_files(file_list, output_file):
     with tarfile.open(output_file, "w:gz") as tf:
         for file_name in file_list:
             tf.add(file_name)
+
+
+def compress_module(directory):
+    parser = OratioIgnoreParser()
+    oi = os.path.join(directory, ".oratio-ignore")
+    if os.path.isfile(oi):
+        parser.load(oi)
+    compress_files(parser.list_files(".")[0], "oratiomodule.tar.gz")
